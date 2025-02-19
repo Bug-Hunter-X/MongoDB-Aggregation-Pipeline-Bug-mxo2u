c27faca2adfcccbc3488210062864ec53821a828
BugHunter-X <askzenai@gmail.com>
2025-02-19 08:14:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,20 @@
+```javascript
+//Corrected aggregation pipeline
+db.collection.aggregate([
+  {
+    $match: { field: 'value' }
+  },
+  {
+    $group: { _id: '$field', count: { $sum: 1 } }
+  },
+  {
+    $sort: { count: -1 }
+  },
+  {
+    $limit: 10 }
+  },  
+  {
+    $project: { _id: 0, field: '$_id', count: 1 }
+  }
+])
+```
